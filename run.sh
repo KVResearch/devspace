b7e0896390f649ab4bd75ecf7557e9903cf0eb35
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2022-11-04 23:04:52 +0000

    feat: add sh

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Copyright (R) KevinZonda Research 2022
+
+V=4.8.2
+CS=$HOME/.local/lib/code-server-$V
+SRC=$CS/src
+
+mv $SRC/browser $SRC/browser-bak
+cp -r ./code-server $SRC
+mv $SRC/code-server $SRC/browser
+
